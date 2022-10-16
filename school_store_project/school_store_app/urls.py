9a4f7a80f86758aad24b025587ec7ba827ba1e3d
swapnana86 <107855889+swapnana86@users.noreply.github.com>
2022-10-16 15:31:44 +0530

    Signup and login added

--- school_store_project/school_store_app/urls.py
+++ school_store_project/school_store_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('signup', views.signup, name="signup"),
+    path('signin', views.signin, name="signin"),
+    #path('logout', views.logout, name="logout"),
+   # path('user_registration/<int:id>/', views.user_registration, name="user_registration")
 ]
--- school_store_project/school_store_app/views.py
+++ school_store_project/school_store_app/views.py
@@ -1,6 +1,52 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome")
+    return render(request, 'home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        c_password = request.POST['c_password']
+
+        details = User.objects.create_user(username, password, c_password)
+
+        details.save()
+        messages.success(request, 'Your account has been created Successfully')
+        return redirect('signin')
+    return render(request, 'register.html')
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            print(" signin")
+            print(user)
+            print(user.id)
+            return render(request, 'user_registration.html', {'user': user})
+        else:
+            messages.error(request, 'Wrong Credentials')
+            return render(request, 'login.html')
+
+    return render(request, 'login.html')
+
+
+def user_registration(request):
+    print("in user reg")
+    print(request)
+
+    return render(request, 'user_registration.html')
+
+
+def logout(request):
+    logout()
