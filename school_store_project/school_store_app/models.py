e0b73431515a51abc5360ed6f889f51fb67c44ed
swapnana86 <107855889+swapnana86@users.noreply.github.com>
2022-10-17 17:44:05 +0530

    add string function

--- school_store_project/school_store_app/models.py
+++ school_store_project/school_store_app/models.py
@@ -5,15 +5,20 @@ from django.db import models
 class Department(models.Model):
     dept_name = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.dept_name
+
 
 class Course(models.Model):
     dept_id = models.ForeignKey(Department, on_delete=models.CASCADE)
     course_name = models.CharField(max_length=250)
 
+   #def __str__(self):
+       #return self.course_name
+
 
 class User(models.Model):
-    username = models.CharField(max_length=250, blank=False)
-    password = models.CharField(max_length=250)
+    #user_id=models.ForeignKey(auth_user,on_delete=models.CASCADE)
     name = models.CharField(max_length=250)
     email = models.EmailField(blank=False)
     dob = models.DateTimeField()
@@ -22,3 +27,6 @@ class User(models.Model):
     address = models.TextField(max_length=250)
     dept_id = models.ForeignKey(Department, on_delete=models.CASCADE)
     course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
