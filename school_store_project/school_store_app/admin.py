2bd100a4466006b9b159bf65f2ba238307d44570
swapnana86 <107855889+swapnana86@users.noreply.github.com>
2022-10-17 17:39:24 +0530

    admin register table

--- school_store_project/school_store_app/admin.py
+++ school_store_project/school_store_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Department, Course
+
 
 # Register your models here.
+
+
+admin.site.register(User)
+admin.site.register(Department)
+admin.site.register(Course)
